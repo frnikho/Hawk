63cf23d06f05492046adaedf50703aea4fe8aecc
Nicolas SANS <nicolas.sansd@gmail.com>
2021-11-04 11:13:25 +0100

    initial commit

---
+++ server/src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const port = process.env.PORT || 4001;
+const index = require("./routes/index");
+
+const app = express();
+app.use(index);
+
+const server = http.createServer(app);
+
+const io = socketIo(server);
+
+const getApiAndEmit = "TODO";
+
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`)
+});

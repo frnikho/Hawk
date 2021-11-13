953ec43adf4419177f25830408313e6afb75e917
JusteUn <57758303+JusteUn@users.noreply.github.com>
2021-11-13 17:35:58 +0100

    create basic home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,34 +1,81 @@
 import React from "react";
+import Container from '@mui/material/Container';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import {Avatar, Typography} from "@mui/material";
+import PersonIcon from '@mui/icons-material/Person';
+import Box from '@mui/material/Box';
+import theme from '../theme'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 function Home() {
     return (
-        <div className="container">
-            <div className="row m-5">
-                <div className="col">
-                    <h1 className="text-center text-white">Hawk</h1>
-                </div>
-            </div>
-            <div className="row justify-content-center m-5">
-                <div className="col-sm-5">
-                    <div className="container">
-                        <div className="row m-5">
-                            <div className="col">
-                                <h1 className="text-center">Play</h1>
-                            </div>
-                            <div className="col">
-                                <form>
-                                    <div className="form-group">
-                                        <label>Pseudo</label>
-                                        <input type="text" className="form-control" placeholder="Taper un pseudo"></input>
-                                    </div>
-                                    <button type="submit" className="btn btn-primary">Create room</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline/>
+                <Box
+                    sx={{
+                        marginTop: 20,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        background: '#2e86de',
+                    }}
+                >
+                    <Avatar sx={{ m:2, bgcolor: '#ff6b6b', width: 100, height: 100}}>
+                        <PersonIcon/>
+                    </Avatar>
+                    <Typography variant="h3">
+                        Play
+                    </Typography>
+                    <Box component="form"
+                         sx={{
+                             m: 3,
+                             display: 'flex',
+                             flexDirection: 'column',
+                             alignItems: 'center',
+                         }}>
+                        <TextField
+                            autoComplete="given-name"
+                            name="firstName"
+                            required
+                            id="firstName"
+                            label="Pseudo"
+                            autoFocus
+                            variant="standard"
+                        />
+                        <Box
+                            sx={{
+                                marginTop: 1,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="secondary"
+                                sx={{ mt: 2, mb: 2 }}
+                            >
+                                Create room
+                            </Button>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="success"
+                                sx={{ mt: 1, mb: 2 }}
+                            >
+                                Join room
+                            </Button>
+                        </Box>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
     );
 }
 

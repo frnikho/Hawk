a88abe018dc0cfe207b471a1fbeb6d4317129a3d
JusteUn <57758303+JusteUn@users.noreply.github.com>
2021-11-13 15:08:09 +0100

    Create theme.js to handle app theme

---
+++ client/src/theme.js
@@ -0,0 +1,11 @@
+import {createTheme} from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        background: {
+            default: '#222f3e',
+        }
+    }
+});
+
+export default theme;

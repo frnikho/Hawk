ecf72392e3e38d49ec7869df7426fd2c1cef459e
Nicolas SANS <nicolas.sansd@gmail.com>
2022-03-16 15:46:06 +0100

    updated import from DBService in server

--- server/src/services/DBService.ts
+++ server/src/services/DBService.ts
@@ -1,12 +1,10 @@
 const mariadb = require('mariadb');
 require('dotenv').config();
 
-const pool = mariadb.createPool({
+export const pool = mariadb.createPool({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         database: process.env.DB_NAME,
         password: process.env.DB_PASSWORD,
         port: process.env.DB_PORT,
     });
-
-module.exports = {pool};

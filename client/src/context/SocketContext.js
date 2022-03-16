0e3c0d7cf70f15af5a07692c5d86e72ce0520117
Nicolas SANS <nicolas.sansd@gmail.com>
2022-03-16 15:58:18 +0100

    edited endpoint

--- client/src/context/SocketContext.js
+++ client/src/context/SocketContext.js
@@ -1,7 +1,7 @@
 import React from "react";
 import socketIOClient from "socket.io-client";
 
-const ENDPOINT = "http://146.59.236.69:4001";
+const ENDPOINT = "https://146.59.236.69:4001";
 
 export const socket = socketIOClient(ENDPOINT);
 export const SocketContext = React.createContext(undefined);
